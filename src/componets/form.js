a732edb4f04877428be1646e0c7cdcf752a26a96
Billi Ramirez <ramirezbilly42@outlook.com>
2020-09-28 09:16:53 -0600

    creating the form, mutations, fetching

---
+++ src/componets/form.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "./form.module.css";
+
+const Form = ({ reloadTodos }) => {
+  const [text, setText] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    if (text === "") {
+      setSubmitting(false);
+      return;
+    }
+
+    await axios.post("/api/create-todo", { text });
+
+    setText("");
+    reloadTodos();
+    setSubmitting(false);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className={styles.label}>
+        Add a Todo
+        <input
+          type="text"
+          className={styles.input}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          disabled={isSubmitting}
+        />
+      </label>
+      <button className={styles.button} disabled={isSubmitting}>
+        Save Todo
+      </button>
+    </form>
+  );
+};
+
+export default Form;
